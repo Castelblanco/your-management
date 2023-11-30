3adc1855bb5ff537a2be9be64fdce551361e77e2
Castelblanco <ejcastelg@gmail.com>
2023-11-30 17:57:14 -0500

    feat: enrutando nuevos modulos a la version 1

--- src/app/routers/v1/index.ts
+++ src/app/routers/v1/index.ts
@@ -1,9 +1,13 @@
+import { departamentsRouters } from '@departaments/infrastructure/routers';
 import { statusCodeRouters } from '@status_codes/infrastructure/routers';
+import { userRolesRouters } from '@user_roles/infrastructure/routers';
 import Elysia from 'elysia';
 
 export const routerV1 = new Elysia();
 
 routerV1.group('v1', (group) => {
     group.use(statusCodeRouters);
+    group.use(userRolesRouters);
+    group.use(departamentsRouters);
     return group;
 });
