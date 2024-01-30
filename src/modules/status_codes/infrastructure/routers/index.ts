8faa203f375eca39c597a93d4b315b7516668c11
Castelblanco <ejcastelg@gmail.com>
2024-01-30 09:54:13 -0500

    feat: añadiendo validacion de status code

--- src/modules/status_codes/infrastructure/routers/index.ts
+++ src/modules/status_codes/infrastructure/routers/index.ts
@@ -3,6 +3,7 @@ import { StatusCodeController } from '../controllers';
 import { StatusCodeServices } from '@status_codes/app/services';
 import { StatusCodePrismaRepository } from '../storages/prisma/implementations';
 import { createId } from '../tools/create_id';
+import { validStatusCode, validStatusCodeList } from '../middleware/valid_status_code';
 
 const controller = new StatusCodeController(
     new StatusCodeServices(new StatusCodePrismaRepository(), createId),
@@ -13,9 +14,15 @@ export const statusCodeRouters = new Elysia();
 statusCodeRouters.group('/status-code', (app) => {
     app.get('get-all', controller.findAll);
     app.get('get-one/:id', controller.findOne);
-    app.post('create-one', controller.createOne);
-    app.post('create-many', controller.createMany);
-    app.put('update-one/:id', controller.updateOne);
+    app.post('create-one', controller.createOne, {
+        body: validStatusCode,
+    });
+    app.post('create-many', controller.createMany, {
+        body: validStatusCodeList,
+    });
+    app.put('update-one/:id', controller.updateOne, {
+        body: validStatusCode,
+    });
     app.delete('delete-one/:id', controller.deleteOne);
 
     return app;
