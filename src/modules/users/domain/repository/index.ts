fbdf6f4237bab29a5d3e2ad21290303a4441ea5e
Castelblanco <ejcastelg@gmail.com>
2024-02-09 17:38:01 -0500

    fix: cambioando logina de users

--- src/modules/users/domain/repository/index.ts
+++ src/modules/users/domain/repository/index.ts
@@ -2,12 +2,7 @@ import type { TUserDOM, TUserFilterDOM, TUserOPT } from '../entities';
 
 export type TUsersRepository = {
     findAll: (filter: TUserFilterDOM, options: TUserOPT) => Promise<TUserDOM[]>;
-    findOne: (
-        filter: TUserFilterDOM,
-        pointSale?: boolean,
-        role?: boolean,
-    ) => Promise<TUserDOM>;
-    findById: (id: string, pointSale?: boolean, role?: boolean) => Promise<TUserDOM>;
+    findOne: (id: string, pointSale?: boolean, role?: boolean) => Promise<TUserDOM>;
     createOne: (user: TUserDOM) => Promise<TUserDOM>;
     updateOne: (user: TUserDOM) => Promise<TUserDOM>;
     deleteOne: (id: string) => Promise<void>;
