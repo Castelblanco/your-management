e98bfb92b0a3e41d8f0e8e0e80c22fc1be5e30c1
Castelblanco <ejcastelg@gmail.com>
2024-05-01 09:52:32 -0500

    fix: optimizando creacion de datos por defecto

--- src/app/db/prisma/connect.ts
+++ src/app/db/prisma/connect.ts
@@ -3,7 +3,8 @@ import { prismaError as PrismaError } from 'prisma-better-errors';
 import {
     createDefaultAllStatus,
     createDefaultAllUserRoles,
-} from './default_types_status';
+    createDefaultGuideServicesAndNovelty,
+} from './default_values';
 
 export const prisma = new PrismaClient();
 export class PrismaRequestError extends Prisma.PrismaClientKnownRequestError {}
@@ -12,8 +13,12 @@ export { PrismaError };
 export const connectPrisma = async (): Promise<void> => {
     try {
         await prisma.$connect();
-        await createDefaultAllStatus();
-        await createDefaultAllUserRoles();
+        /* eslint-disable @typescript-eslint/no-confusing-void-expression */
+        await Promise.allSettled([
+            await createDefaultAllStatus(),
+            await createDefaultAllUserRoles(),
+            await createDefaultGuideServicesAndNovelty(),
+        ]);
         console.log('Prisma Conected');
     } catch (e) {
         console.log(e);
--- src/app/db/prisma/default_values.ts
+++ src/app/db/prisma/default_values.ts
@@ -115,3 +115,67 @@ export const createDefaultAllUserRoles = async () => {
         console.log('Error create default user roles');
     }
 };
+
+export const createDefaultGuideServicesAndNovelty = async () => {
+    try {
+        const [novelty, services] = await Promise.all([
+            prisma.guide_Service_Novelty.count(),
+            prisma.guide_Service_Type.count(),
+        ]);
+
+        if (novelty === 0) {
+            await prisma.guide_Service_Novelty.createMany({
+                data: [
+                    {
+                        id: crypto.randomUUID(),
+                        name: 'Direccion no existe',
+                    },
+                    {
+                        id: crypto.randomUUID(),
+                        name: 'Cliente no se encuentra',
+                    },
+                    {
+                        id: crypto.randomUUID(),
+                        name: 'Cliente no tiene dinero para pagar contraentrega',
+                    },
+                    {
+                        id: crypto.randomUUID(),
+                        name: 'Coordinar entrega',
+                    },
+                ],
+            });
+            console.log('Create Default Guides Novelty');
+        }
+
+        if (services === 0) {
+            await prisma.guide_Service_Type.createMany({
+                data: [
+                    {
+                        id: crypto.randomUUID(),
+                        tab: 'DE',
+                        name: 'DOCUMENTO EXPRESS',
+                    },
+                    {
+                        id: crypto.randomUUID(),
+                        tab: 'MA',
+                        name: 'MERCANCIA AEREA',
+                    },
+                    {
+                        id: crypto.randomUUID(),
+                        tab: 'MT',
+                        name: 'MERCANCIA TERRESTRE',
+                    },
+                    {
+                        id: crypto.randomUUID(),
+                        tab: 'PE',
+                        name: 'PAQUETE EXPRESS',
+                    },
+                ],
+            });
+            console.log('Create Default Guides Services Type');
+        }
+    } catch (e) {
+        console.log({ e });
+        console.log('Error create default guides services and novelty');
+    }
+};
